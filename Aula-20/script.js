d166abcbf0f69dcd9b3564264c13033b3507d152
Marcos Antonio Concegliere <markin1208@hotmail.com>
2025-05-22 10:46:39 -0300

    Add files via upload

--- Aula-20/script.js
+++ Aula-20/script.js
@@ -1,29 +1,34 @@
-function mostraridade() {
-    var idade;
-    idade = document.getElementById("idade").value;
+function calcular() {
+    var idade = document.getElementById("idade").value;
+
+    if (idade == ''){
+        alert('Não digitou nada');
+        document.getElementById ('mostrar').innerHTML = 'Não digitou nada';}
+    else{
+        idade = Number(idade);
+    }
+   
     if (idade < 0){
-        alert ('Idade inválida')
+        document.getElementById ('mostrar').innerHTML = 'idade inválida';
     }
-    else if (idade = 0){
-        alert ('recém nascido')
+    else if (idade === 0){
+        document.getElementById ('mostrar').innerHTML = 'recém nascido';
     }
-    else if (idade > 0 && idade < 10){
-        alert ('criança')
+    else if (idade > 0 && idade <= 9){
+         document.getElementById ('mostrar').innerHTML = 'criança';
     }
-    else if (idade > 9 && idade < 16){
-        alert ('adolescente')
+    else if (idade >= 10 && idade <= 15){
+         document.getElementById ('mostrar').innerHTML = 'adolescente';
     }
-    else if (idade > 15 && idade < 25){
-        alert ('jovem')
+    else if (idade >= 16 && idade <= 25){
+         document.getElementById ('mostrar').innerHTML = 'jovem';
     }
-    else if (idade > 24 && idade < 60){
-        alert ('adulto')
+    else if (idade >= 26 && idade < 60){
+         document.getElementById ('mostrar').innerHTML = 'adulto';
     }
     else if (idade > 59){
-        alert ('idoso')
+         document.getElementById ('mostrar').innerHTML = 'idoso';
     }
-    else if (idade = ''){
-        alert ('Não digitou nada')
-    }
+    
 }
 
