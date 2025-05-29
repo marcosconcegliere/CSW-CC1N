9bc7492ac76d8d3bfd2c88ec9686a306c989946c
Marcos Antonio Concegliere <markin1208@hotmail.com>
2025-05-28 22:24:25 -0300

    Add files via upload

---
+++ Aula-21/script.js
@@ -0,0 +1,10 @@
+// for (x=50; x>=0; x-=2){
+//     console.log("Hello World " + x);
+// }
+
+var y = 0
+while (y < 10) {
+    console.log ("Hello World " + y);
+    y+=1
+}
+console.log("terminei");
